7a26ef11ac63463eff156f6890b3da9b44f35d68
James <chenjames215@gmail.com>
2025-07-08 16:35:51 +0800

    build: modify listening url

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,6 @@ app.use((_req, _res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on port ${PORT.toString()}`);
 });
